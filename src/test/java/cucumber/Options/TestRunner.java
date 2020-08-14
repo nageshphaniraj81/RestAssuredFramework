df442843befb102b004848b55feed6d5dcfde01a
nagesh <nageshphaniraj81@gmail.com>
2020-08-15 00:21:16 +0530

    Update to TestRunner File

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,10 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinations" }, tags = { "@AddPlace","@DeletePlace"})
-@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinations" }, plugin = "json:target/jsonReports/cucumber-report.json")
+
+@CucumberOptions(features = "src/test/java/features", glue = {
+		"stepDefinations" }, plugin = "json:target/jsonReports/cucumber-report.json")
 
 public class TestRunner {
-	
+
+	// @CucumberOptions(features = "src/test/java/features", glue = {
+	// "stepDefinations" }, tags = { "@AddPlace","@DeletePlace"})
 
 }
